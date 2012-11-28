0f6a152e5c861ae565915cb930ded546244465fc
David King <dking@ketralnis.com>
2012-11-28 15:32:27 -0800

    Rename ftssearch.py -> fts.py

--- fts.py
+++ fts.py
@@ -1,15 +1,39 @@
-import logging
+#!/usr/bin/env python
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+import sys
+import os
 
-from db import conn
-from db import get_or_createdb
-from ftssync import sync
+from db import finddb, prefix_expr
+
+def search(conn, prefix, term):
+    # TODO: do a pass over the results to see if the files still exist and if
+    # last_modified has changed
+
+    # TODO: get matchinfo and optionally display matches
+
+    # TODO: cmdline switches to display the full path instead of the relative path
+
+    with conn:
+        c = conn.cursor()
+        try:
+            prefix = prefix or ''
+            prefixexpr = prefix_expr(prefix)
+            c.execute("""
+                SELECT path
+                  FROM files f, files_fts ft
+                 WHERE f.docid = ft.docid
+                   AND (? = '' OR f.path LIKE ? ESCAPE '\\') -- use the prefix if present
+                   AND ft.body MATCH ?
+           """, (prefix, prefixexpr, term,))
+            for (path, ) in c:
+                yield path[len(prefix)+1:]
+        finally:
+            c.close()
 
 def main():
-    dbid = get_or_createdb(conn.cursor(), "mydb")
-    sync(dbid, "/Users/dking/src/fts/rando")
+    root, prefix, conn = finddb(os.getcwd())
+    for fname in search(conn, prefix, sys.argv[1]):
+        print fname
 
 if __name__ == '__main__':
     main()
