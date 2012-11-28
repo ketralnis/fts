86546dbd86ea3eaa600882dd05293b05510575f4
David King <dking@ketralnis.com>
2012-11-28 15:47:14 -0800

    Add a warning to fts.py for when files are out of date

--- fts.py
+++ fts.py
@@ -2,7 +2,9 @@
 
 import sys
 import os
+import stat
 
+from db import logger
 from db import finddb, prefix_expr
 
 def search(conn, prefix, term):
@@ -19,14 +21,33 @@ def search(conn, prefix, term):
             prefix = prefix or ''
             prefixexpr = prefix_expr(prefix)
             c.execute("""
-                SELECT path
+                SELECT f.path, f.last_modified
                   FROM files f, files_fts ft
                  WHERE f.docid = ft.docid
                    AND (? = '' OR f.path LIKE ? ESCAPE '\\') -- use the prefix if present
                    AND ft.body MATCH ?
            """, (prefix, prefixexpr, term,))
-            for (path, ) in c:
-                yield path[len(prefix)+1:] if prefix else path
+            for (path, last_modified) in c:
+
+                if prefix:
+                    assert path.startswith(prefix)
+
+                shortpath = path[len(prefix)+1:] if prefix else path
+
+                needsync = 0
+
+                try:
+                    st = os.stat(shortpath)
+                    if int(st[stat.ST_MTIME]) > last_modified:
+                        needsync += 1
+                except OSError:
+                    needsync += 1
+
+                yield shortpath
+
+            if needsync:
+                logger.warning("%d files were missing or out-of-date, you may need to resync", needsync)
+
         finally:
             c.close()
 
